f6473d8580c5f97013e966f2d151a81b8368dd70
turbovix <Turbovix@users.noreply.github.com>
2024-08-25 07:42:37 -0300

    Update LogoImage.jsx
    Altera o tamanho do logo na tela de login.

--- src/login/LogoImage.jsx
+++ src/login/LogoImage.jsx
@@ -7,8 +7,8 @@ import Logo from '../resources/images/logo.svg?react';
 const useStyles = makeStyles((theme) => ({
   image: {
     alignSelf: 'center',
-    maxWidth: '240px',
-    maxHeight: '120px',
+    maxWidth: '440px',
+    maxHeight: '320px',
     width: 'auto',
     height: 'auto',
     margin: theme.spacing(2),
